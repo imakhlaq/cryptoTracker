03c07f26a651bb3d11227478ecb9e84c3e7a0742
imakhlaq <imakhlaqXD@gmail.com>
2023-01-29 03:26:26 +0530

    got trending movies

--- src/components/banner/Carousel.tsx
+++ src/components/banner/Carousel.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { fetchTrendingCoins } from "../../store/cryptoSlice/cryptoSlice";
 import AliceCarousel from "react-alice-carousel";
 import { Link } from "react-router-dom";
+import { numberWithCommas } from "../../utils/priseWithCommas";
 const Carousel = () => {
   const dispatch = useAppDispatch();
   const trendingCoins = useAppSelector((state) => state.crypto.trendingCoins);
@@ -16,11 +17,8 @@ const Carousel = () => {
   const items = trendingCoins.map((coin) => {
     let profit = coin.price_change_24h >= 0;
     return (
-      <Link
-        className="flex justify-center items-center gap-8 md:gap-7"
-        to={`/coins/${coin.id}`}
-      >
-        <div className="flex flex-col gap-3 justify-center items-center">
+      <Link className="" to={`/coins/${coin.id}`}>
+        <div className="flex flex-col gap-2 justify-center items-center">
           <img src={coin.image} alt={coin.name} className="h-20" />
           <div className="flex justify-center items-center gap-3">
             <span className="text-[gold]">{coin.symbol}</span>
@@ -28,9 +26,9 @@ const Carousel = () => {
               {profit && "+"} {coin.price_change_percentage_24h.toFixed(2)}
             </span>
           </div>
-          <div className="flex items-center justify-center gap-2 text-xl font-bold lg:text-2xl">
+          <div className="flex items-center justify-center gap-2 text-base md:text-base font-bold lg:text-2xl">
             <span>{symbol}</span>
-            <span>{coin.current_price.toFixed(2)}</span>
+            <span>{numberWithCommas(coin.current_price.toFixed(2))}</span>
           </div>
         </div>
       </Link>
@@ -46,7 +44,7 @@ const Carousel = () => {
   };
 
   return (
-    <div className="max-w-sm mt-20 md:max-w-2xl lg:max-w-4xl text-center ">
+    <div className="max-w-[26rem] mt-20 md:max-w-3xl lg:max-w-4xl text-center ">
       <AliceCarousel
         mouseTracking
         infinite
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440pxx",
+    },
     extend: {
       fontFamily: {
         sans: ["Montserrat", "sans-serif"],
