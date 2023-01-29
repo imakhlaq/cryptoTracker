9ca7aa63a2accc761aa330b456fc9b83e08eff27
imakhlaq <imakhlaqXD@gmail.com>
2023-01-29 13:06:40 +0530

    search bar

--- src/components/CoinTable.tsx
+++ src/components/CoinTable.tsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 const CoinTable = () => {
+  const [search, setSearch] = useState("");
+
+  const searchHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
+  console.log(search);
+
   return (
     <section className="mt-10">
       <div className="flex flex-wrap justify-center items-center mx-3 space-y-7 flex-col">
@@ -6,6 +16,7 @@ const CoinTable = () => {
           Cryptocurrency Prices by Market Cap
         </h2>
         <input
+          onChange={searchHandler}
           type="text"
           placeholder="Search For a Crypto Currency"
           className="w-full bg-[#14161a] px-3 py-4 rounded-md focus:outline-none text-white placeholder:text-[#494949] placeholder:text-xl placeholder:font-thin border border-[#494949] md:max-w-lg"
