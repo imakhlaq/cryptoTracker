49d97a1501e5fb621a5c5f5d40f15a38277b1a46
imakhlaq <imakhlaqXD@gmail.com>
2023-01-30 18:09:55 +0530

    almost done

--- src/components/CoinInfo.tsx
+++ src/components/CoinInfo.tsx
@@ -29,7 +29,8 @@ const CoinInfo = () => {
         ? `${date.getHours() - 12}:${date.getMinutes()} AM`
         : `${date.getHours()}:${date.getMinutes()} PM`;
 
-    return days === 1 ? time : date;
+    const years = new Intl.DateTimeFormat("en-US").format(date);
+    return days === 1 ? time : years;
   });
   const chartjsData = prices.map((coin) => coin[1]);
   return (
@@ -52,7 +53,9 @@ const CoinInfo = () => {
       <div className="flex gap-6 justify-around mt-9">
         {chartDays.map((btn) => (
           <button
-            className="py-2 px-4 active:bg-yellow-500 md:px-6 bg-[gray] rounded-md"
+            className={`py-2 px-4 md:px-6 bg-[gray] rounded-md ${
+              days == btn.value ? "bg-yellow-500" : "bg-[gray]"
+            }`}
             onClick={() => setDays(btn.value)}
           >
             {btn.label}
