968050159e412f81718e7f5c91563560162ace4a
imakhlaq <imakhlaqXD@gmail.com>
2023-01-30 18:26:48 +0530

    almost done

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,15 +1,7 @@
 const Loading = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div
-        className="
-      spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0
-        text-yellow-500
-      "
-        role="status"
-      >
-        <span className="">Loading...</span>
-      </div>
+    <div className="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2">
+      <div className="border-t-transparent border-solid animate-spin  rounded-full border-[gold] border-8 h-64 w-64"></div>
     </div>
   );
 };
