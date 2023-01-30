06e5141dab6e4e1b8977f0311f8cc69e09a74e85
imakhlaq <imakhlaqXD@gmail.com>
2023-01-30 19:29:20 +0530

    authentication

--- src/components/authentication/AuthModal.tsx
+++ src/components/authentication/AuthModal.tsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
 const AuthModal = () => {
-  return <div>AuthModal</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <button
+      onClick={() => setOpen(true)}
+      className="p-2 md:px-4 bg-[gold] rounded-md text-black font-semibold"
+    >
+      SignIn
+    </button>
+  );
 };
 export default AuthModal;
