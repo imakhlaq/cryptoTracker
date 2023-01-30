629c419e1d1f521ad1c42b81fcdd925a2e536308
imakhlaq <imakhlaqXD@gmail.com>
2023-01-30 17:19:02 +0530

    charjs

--- src/components/CoinInfo.tsx
+++ src/components/CoinInfo.tsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
-import { CategoryScale, Chart, LinearScale } from "chart.js";
 import { useParams } from "react-router-dom";
 import { fetchCoinHistory } from "../store/cryptoSlice/cryptoSlice";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { Chart, PointElement } from "chart.js/auto";
 
 const CoinInfo = () => {
   const { id } = useParams();
   const dispatch = useAppDispatch();
   const currency = useAppSelector((state) => state.crypto.currency);
-  const data = useAppSelector((state) => state.crypto.history);
+  const { prices } = useAppSelector((state) => state.crypto.history);
   const loading = useAppSelector((state) => state.crypto.loading);
   const [days, setDays] = useState<number>(365);
 
@@ -17,11 +17,31 @@ const CoinInfo = () => {
     dispatch(fetchCoinHistory({ id, currency, days }));
   }, [currency, days]);
 
-  Chart.register(CategoryScale, LinearScale);
-
   if (loading) {
     return <p>Loading</p>;
   }
-  return <div></div>;
+  Chart.register(PointElement);
+  const labels = prices.map((coin) => {
+    let date = new Date(coin[0]);
+    let time =
+      date.getHours() > 12
+        ? `${date.getHours() - 12} : ${date.getMinutes()}`
+        : `${date.getHours()}:${date.getMinutes()}`;
+
+    return days === 1 ? time : date;
+  });
+
+  const chartjsData = prices.map((coin) => coin[1]);
+
+  console.log(chartjsData);
+
+  return (
+    <Line
+      data={{
+        labels: labels,
+        datasets: [{ data: chartjsData, label: "History" }],
+      }}
+    />
+  );
 };
 export default CoinInfo;
