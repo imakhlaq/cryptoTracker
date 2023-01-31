8b9a258051de728e13008860836381ea7e624664
imakhlaq <imakhlaqXD@gmail.com>
2023-01-31 18:19:48 +0530

    authentication

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,8 +4,14 @@ const Profile = () => {
   const user = useAppSelector((state) => state.auth.user);
 
   return (
-    <div className="h-10 w-10 bg-slate-50 rounded-full cursor-pointer">
-      <img src={user?.picture ?? "dadadad"} alt="" />
+    <div className="h-10 w-10 bg-slate-50 rounded-full cursor-pointer overflow-hidden">
+      <img
+        src={
+          user?.picture ??
+          "https://secure.gravatar.com/avatar/03dcd7ac0ca81f33eecc10f5f434ca91/?s=48&d=https://images.binaryfortress.com/General/UnknownUser1024.png"
+        }
+        alt={user?.userName ?? user?.email ?? "profile picure"}
+      />
     </div>
   );
 };
